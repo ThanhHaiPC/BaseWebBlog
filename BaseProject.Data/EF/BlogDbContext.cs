4f6ff464dafb600c6b128c112cac81d36a88e859
ThanhHaiPC <ThanhHaiPC2002@gmail.com>
2023-09-29 16:38:10 +0700

    Add Entity

---
+++ BaseProject.Data/EF/BlogDbContext.cs
@@ -0,0 +1,40 @@
+﻿using BaseProject.Data.Configurations;
+using BaseProject.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseProject.Data.EF
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoriesDetail> CategoriesDetail { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Follow> Follows { get; set; }
+        public DbSet<Images> Images { get; set; }
+        public DbSet<Like> likes { get; set; }
+        public DbSet<Posts> Posts { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public DbSet<Reply> Replies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<AppConfig> AppConfigs { get; set; }    
+    }
+}
---
+++ BaseProject.Data/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseProject.Data.Entities
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public int PostID { get; set; }
+        public Guid UserId { get; set; }
+        public string Content { get; set; }
+        public int Like { get; set; }
+        public DateTime Date { get; set; }
+
+        // Relationship
+        public Posts Post { get; set; }
+        public User User { get; set; }
+    }
+}
